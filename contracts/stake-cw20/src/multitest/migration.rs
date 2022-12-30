7e77db41dcdf14d8606385b9b1bb95a61e759b85
Jakub Bogucki <jakub@confio.gmbh>
2022-12-30 14:10:18 +0100

    Fix multitest suite

--- contracts/stake-cw20/src/multitest/migration.rs
+++ contracts/stake-cw20/src/multitest/migration.rs
@@ -1,25 +1,17 @@
+use anyhow::Result as AnyResult;
+
 use crate::msg::{
     ExecuteMsg, GetConfigResponse, QueryMsg, ReceiveMsg, StakedBalanceAtHeightResponse,
     StakedValueResponse, TotalStakedAtHeightResponse, TotalValueResponse,
 };
-use crate::state::MAX_CLAIMS;
 use crate::ContractError;
-use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-use cosmwasm_std::{to_binary, Addr, Empty, MessageInfo, Uint128};
+
+use cosmwasm_std::{to_binary, Addr, Uint128};
 use cw20::Cw20Coin;
+use cw_multi_test::{next_block, App, AppResponse, Contract, ContractWrapper, Executor};
 use cw_utils::Duration;
 
-use cw_multi_test::{next_block, App, AppResponse, Contract, ContractWrapper, Executor};
-
-use anyhow::Result as AnyResult;
-
-use cw_controllers::{Claim, ClaimsResponse};
-use cw_utils::Expiration::AtHeight;
-
-const ADDR1: &str = "addr0001";
-const ADDR2: &str = "addr0002";
-const ADDR3: &str = "addr0003";
-const ADDR4: &str = "addr0004";
+pub const UNBONDING_DURATION: u64 = 86_400 * 14; // two weeks
 
 pub fn store_staking(app: &mut App) -> u64 {
     let contract = Box::new(ContractWrapper::new(
@@ -30,7 +22,7 @@ pub fn store_staking(app: &mut App) -> u64 {
     app.store_code(contract)
 }
 
-pub fn store_cw20(app: &mut App) -> Box<dyn Contract<Empty>> {
+pub fn store_cw20(app: &mut App) -> u64 {
     let contract = Box::new(ContractWrapper::new(
         cw20_base::contract::execute,
         cw20_base::contract::instantiate,
@@ -40,11 +32,15 @@ pub fn store_cw20(app: &mut App) -> Box<dyn Contract<Empty>> {
 }
 
 #[derive(Debug)]
-struct SuiteBuilder {}
+struct SuiteBuilder {
+    initial_balances: Vec<Cw20Coin>,
+}
 
 impl SuiteBuilder {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            initial_balances: vec![],
+        }
     }
 
     #[track_caller]
@@ -56,18 +52,18 @@ impl SuiteBuilder {
         let token_contract = app
             .instantiate_contract(
                 cw20_code_id,
-                Addr::unchecked(owner),
+                owner.clone(),
                 &cw20_base::msg::InstantiateMsg {
                     name: String::from("Test"),
                     symbol: String::from("TEST"),
                     decimals: 6,
-                    initial_balances,
+                    initial_balances: self.initial_balances,
                     mint: None,
                     marketing: None,
                 },
                 &[],
                 "cw20-token",
-                Some(owner),
+                Some(owner.to_string()),
             )
             .unwrap();
         app.update_block(next_block);
@@ -76,16 +72,16 @@ impl SuiteBuilder {
         let staking_contract = app
             .instantiate_contract(
                 staking_code_id,
-                Addr::unchecked(owner),
+                owner.clone(),
                 &crate::msg::InstantiateMsg {
-                    owner: Some(owner.clone()),
+                    owner: Some(owner.to_string()),
                     manager: Some("manager".to_string()),
                     token_address: token_contract.to_string(),
-                    unstaking_duration,
+                    unstaking_duration: Some(Duration::Time(UNBONDING_DURATION)),
                 },
                 &[],
                 "staking",
-                Some(owner),
+                Some(owner.to_string()),
             )
             .unwrap();
         app.update_block(next_block);
@@ -100,20 +96,8 @@ impl SuiteBuilder {
 }
 
 pub struct Suite {
-    pub owner: String,
+    pub owner: Addr,
     pub app: App,
     pub token_contract: Addr,
     pub staking_contract: Addr,
-}
-
-fn get_balance<T: Into<String>, U: Into<String>>(
-    app: &App,
-    contract_addr: T,
-    address: U,
-) -> Uint128 {
-    let msg = cw20::Cw20QueryMsg::Balance {
-        address: address.into(),
-    };
-    let result: cw20::BalanceResponse = app.wrap().query_wasm_smart(contract_addr, &msg).unwrap();
-    result.balance
 }
